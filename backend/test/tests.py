8c76b5d6748c688e9a3881f96613c0278979b331
mahajanpranjal <mahajan.pran@northeastern.edu>
2025-04-18 11:45:27 -0400

    changing backend code for integration3

--- backend/test/tests.py
+++ backend/test/tests.py
@@ -2,7 +2,12 @@
 
 import pytest
 from fastapi.testclient import TestClient
-from backend.main import app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import app
+
 
 client = TestClient(app)
 
